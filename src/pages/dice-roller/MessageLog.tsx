7f766c13e54f2ad1d537b7eb4dae7df3885db1ab
Jairus Houdek <jairus.houdek@gmail.com>
2026-08-23 19:44:20 -0500

    feat: update hover color in CSS and add MessageLog and MessagePost components for dice roller

---
+++ src/pages/dice-roller/MessageLog.tsx
@@ -0,0 +1,9 @@
+export const MessageLog = () => {
+
+    return (
+        <div className="w-full p-1 m-0">
+            <div className="w-full rounded shadow-lg pt-0 px-2 pb-2 bg-[var(--bg)] border border-[var(--border)]">
+            </div>
+        </div>
+    )
+}
