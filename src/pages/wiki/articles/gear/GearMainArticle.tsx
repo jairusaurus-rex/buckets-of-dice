4a23c3aa0dbb83618e4d41cc375776760fd49b9c
Jairus Houdek <jairus.houdek@gmail.com>
2026-09-03 15:04:09 -0500

    fix: improve clarity in GearMainArticle component by correcting number formatting and phrasing

--- src/pages/wiki/articles/gear/GearMainArticle.tsx
+++ src/pages/wiki/articles/gear/GearMainArticle.tsx
@@ -34,7 +34,9 @@ export const GearArticle = () => {
 
     return (
         <div className="p-2 text-justify">
-            <p className="p-2">Like skills, all gear are paired with a number <DiceText>4</DiceText> through <DiceText>12</DiceText>, <DiceText>4</DiceText> being the lowest quality item and <DiceText>12</DiceText> being the best. They
+            <p className="p-2">Like skills, all gear are paired with a 
+                number <DiceText>4</DiceText>, <DiceText>6</DiceText>, <DiceText>8</DiceText>, <DiceText>10</DiceText>, or <DiceText>12</DiceText>. <DiceText>4</DiceText> being the lowest quality item 
+                and <DiceText>12</DiceText> being the best. They
                 corospond to dice that can be used in a dice pool. A Spacer is more likely able to be successful at a task if their skill is paired
                 with a gear item</p>
             <p className="p-2">Gear are broken into two categories. Suit Gear and Non Suit Gear. Suit Gear are four specific 
@@ -43,7 +45,7 @@ export const GearArticle = () => {
                 Suit Gear are gear items that are carried and used apart of the Star Suit. See Non-Suit Gear page <Link to={`/wiki/${WikiArticleLinks.GEAR_NON_SUIT}`}>here</Link></p>
             <h3 className="font-bold">Buying</h3>
             <p className="p-2">You can spend 10 credits to get a new gear. To buy new gear, the tech level of the area must be at least 2. All new
-                gear starts at Dice <DiceText>4</DiceText>.</p>
+                gear starts at <DiceText>4</DiceText>.</p>
             <p className="p-2">You can only have one Star Suit Accent at a time but you can spend 10 credits to change your accent, it’s dice stays
                 the same. The Tech level of the area you are in must equal the accent dice to do this </p>
             <div  className={`w-1/2 md:w-1/4 float-left m-2 border border-[var(--border)]`}>
